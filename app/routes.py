c30f96841376217ea32638e6a4cd96a9def805c1
Theresa Davis <perugia33@gmail.com>
2022-04-25 10:26:17 -0700

    changes to routes.py

--- app/routes.py
+++ app/routes.py
@@ -8,13 +8,14 @@ class Book:
         self.description = description
 
 books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+    Book(1, "Sketches in Spain Part 1", "An Art Journal and guide."),
+    Book(2, "Sketches in Spain Part 2", "An Art Journal and guide."),
+    Book(3, "Sketches in Spain Part 1", "An Art Journal and guide.")
 ]
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+# GETTING ALL BOOKS
 @books_bp.route("", methods=["GET"])
 def handle_books():
     books_response = []
@@ -27,3 +28,17 @@ def handle_books():
             }
         )
     return jsonify(books_response)
+
+# Getting One Book with its description
+@books_bp.route("/<book_id>", methods=["GET"])	
+
+
+def handle_book(book_id):			
+    book_id = int(book_id) 
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id, 
+                "title": book.title,
+                 "description": book.description }
+            					
